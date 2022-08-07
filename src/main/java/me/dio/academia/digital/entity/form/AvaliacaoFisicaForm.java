9266fd5e8c89803aec18262dd4f63ba6542fc0a4
dmitryrocha <dmitryrocha@gmail.com>
2022-08-07 06:26:44 -0300

    Validações aos CRUDs adicionadas

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -3,16 +3,25 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import me.dio.academia.digital.entity.Aluno;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvaliacaoFisicaForm {
 
+  @Positive(message = "O ID do aluno precisa ser um número positivo")
   private Long alunoId;
 
+  @NotNull(message = "Este campo é obrigatório")
+  @Positive(message = "O peso do aluno precisa ser um número positivo")
   private double peso;
 
+  @NotNull(message = "Este campo é obrigatório")
+  @Positive(message = "A altura do aluno precisa ser um número positivo")
+  @DecimalMin(value = "120", message = "'${validatedValue}' precisa ser no mínimo {value}")
   private double altura;
 }
--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,7 +1,12 @@
 package me.dio.academia.digital.entity.form;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class MatriculaForm {
 
+  @NotNull(message = "Este campo é obrigatório")
+  @Positive(message = "O ID de aluno não pode ter valor negativo")
   private Long alunoId;
 
 }
